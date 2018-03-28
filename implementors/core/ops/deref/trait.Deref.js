588acb5aa5ad76266770f29f3b40571b902822e6
Travis CI User <travis@example.org>
2018-03-28 14:47:34 +0000

    Documentation upload

---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = ["impl&lt;T, S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"actix_web/struct.Path.html\" title=\"struct actix_web::Path\">Path</a>&lt;T, S&gt;","impl&lt;T, S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"actix_web/struct.Query.html\" title=\"struct actix_web::Query\">Query</a>&lt;T, S&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"actix_web/fs/struct.NamedFile.html\" title=\"struct actix_web::fs::NamedFile\">NamedFile</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = ["impl&lt;T, S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"actix_web/struct.Path.html\" title=\"struct actix_web::Path\">Path</a>&lt;T, S&gt;","impl&lt;T, S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"actix_web/struct.Query.html\" title=\"struct actix_web::Query\">Query</a>&lt;T, S&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"actix_web/fs/struct.NamedFile.html\" title=\"struct actix_web::fs::NamedFile\">NamedFile</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
